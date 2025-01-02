7fb78a1c6b015c51b24b4decde75cec7ed4fcef1
Hrishikesh <ghrishikesh77@gmail.com>
2025-01-02 10:11:00 +0530

    fix import paths for Header and Footer components in Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Layout/Header.jsx";
-import Footer from "./components/Layout/Footer.jsx";
+import Header from "./components/layout/Header.jsx";
+import Footer from "./components/layout/Footer.jsx";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { Outlet } from "react-router-dom";
